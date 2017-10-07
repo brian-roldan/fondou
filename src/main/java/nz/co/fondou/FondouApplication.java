4aa07a7b64adc2b83a2d8e41e1a7e0767d49b248
Brian Roldan <brian.roldan15@gmail.com>
2017-10-07 15:19:42 +1300

    Initial Project

---
+++ src/main/java/nz/co/fondou/FondouApplication.java
@@ -0,0 +1,12 @@
+package nz.co.fondou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FondouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FondouApplication.class, args);
+	}
+}
---
+++ src/test/java/nz/co/fondou/FondouApplicationTests.java
@@ -0,0 +1,16 @@
+package nz.co.fondou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FondouApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
