8a673909e4b61bbb5fc6db76fe68199fb2fe9c5e
Brian Roldan <brian.roldan15@gmail.com>
2017-10-09 20:33:45 +1300

    Added configuration for fondou app

---
+++ src/main/java/nz/co/fondou/configuration/FondouConfiguration.java
@@ -0,0 +1,13 @@
+package nz.co.fondou.configuration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class FondouConfiguration {
+
+	private final String mainBranchName;
+	
+}
