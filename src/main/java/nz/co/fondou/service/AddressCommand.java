a034424b70ebd4a85961463ea3e90b5573d1a397
Brian Roldan <brian.roldan15@gmail.com>
2017-10-20 13:45:56 +1300

    Fixed double validation for address form

--- src/main/java/nz/co/fondou/service/AddressCommand.java
+++ src/main/java/nz/co/fondou/service/AddressCommand.java
@@ -3,7 +3,6 @@ package nz.co.fondou.service;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,11 +19,9 @@ public class AddressCommand {
 
 	private String branchName;
 	@NotBlank(message="Address cannot be empty.")
-	@NotEmpty(message="Address cannot be empty.")
 	@Size(max=100, message="Address can only be 100 in length.")
 	private String address;
 	@NotBlank(message="Map link cannot be empty.")
-	@NotEmpty(message="Map link cannot be empty.")
 	@Size(max=200, message="Map link can only be 200 in length.")
 	private String mapLink;
 	
